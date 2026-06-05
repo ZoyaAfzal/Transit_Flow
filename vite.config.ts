7ffa2529da72ae20f72b943c83de73098042c71d
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2026-06-05 16:01:42 -0700

    Transit_Flow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
